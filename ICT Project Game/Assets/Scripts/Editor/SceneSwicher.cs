25d7d5b14a0f3c54c0bf5717cd41f4e5db2ed401
Boudewijn Witteveen <516708@student.glu.nl>
2017-11-14 09:46:34 +0100

    First Commit. Added Unity Project

---
+++ ICT Project Game/Assets/Scripts/Editor/SceneSwicher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SceneSwicher : Editor
+{
+    [MenuItem("Custom Tools/Go to Main Menu")]
+    static void GoToMenu()
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/MainMenu.unity");
+        }
+    }
+
+    [MenuItem("Custom Tools/Go to Main Game Scene")]
+    static void GoToGame()
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Game.unity");
+        }
+    }
+
+    [MenuItem("Custom Tools/Go to Game Over Scene")]
+    static void GoToGameOverScene()
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/GameOver.unity");
+        }
+    }
+
+    [MenuItem("Custom Tools/Go to Test Scene")]
+    static void GoToPrefabTestScene()
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Test.unity");
+        }
+    }
+}
